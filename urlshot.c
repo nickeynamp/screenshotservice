f6da75733d25476d0df9000f09acaa70b6f6f530
Nick Pham <namp@kth.se>
2016-12-19 10:21:45 -0600

    commit new files

---
+++ urlshot.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+  FILE *urls=fopen("URLs.txt","r+");
+  char *buffer=NULL;
+  size_t size=0;
+  while(1){
+    int check =getline(&buffer,&size,urls);
+    if(check==-1) {printf("End end end"); break;}
+
+    printf("Current URL is: %s",buffer);
+  }
+
+  return 0;
+}
